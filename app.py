ba3ba6d4011318d24538736f9805790b5a9cbefa
papasim824 <papasim824@gmai.com>
2023-12-23 01:26:38 +0900

    mod

--- app.py
+++ app.py
@@ -1,19 +1,58 @@
 import const
 import openai
 import streamlit as st
-import streamlit.components.v1 as components
 from modules.create import create
 from modules.play import play
 from modules.settings import settings
 from PIL import Image
-from streamlit_cognito_auth import CognitoAuthenticator
 from streamlit_option_menu import option_menu
+from modules.streamlit_google_oauth import google_oauth2_required
+import os
+
+
+@google_oauth2_required
+def main():
+    if st.session_state.api_key:
+        openai.api_key = st.session_state.api_key
+    else:
+        openai.api_key = st.secrets["OPEN_AI_KEY"]
+    header_cols = st.columns([1,3,1])
+    header_cols[0].image("assets/header.png")
+    header_cols[2].caption(f"logged in {st.session_state.user_email}")
+    selected = option_menu(
+        None,
+        ["よむ", "つくる"],
+        icons=["bi-play-btn", "bi-brush"],
+        menu_icon=None,
+        default_index=0,
+        orientation="horizontal",
+        styles={
+            "container": {
+                "margin": "0px !important",
+                "background-color": "#fafafa",
+            },
+            "icon": {"color": "fafafa", "font-size": "25px"},
+            "nav-link": {
+                "font-size": "20px",
+                "margin": "0px",
+                "--hover-color": "#eee",
+            },
+            "nav-link-selected": {"background-color": "004a55"},
+        },
+    )
+    if selected == "つくる":
+        create()
+    elif selected == "よむ":
+        play()
+    elif selected == "せってい":
+        settings()
 
 if "login" not in st.session_state:
     st.session_state.client = None
     st.session_state.login = False
     st.session_state.guest_login = False
     st.session_state.user_id = ""
+    st.session_state.user_email = ""
     st.session_state.api_key = ""
     st.session_state.disable_audio = False
 
@@ -57,57 +96,4 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-if not st.session_state.login:
-    left_co, cent_co, last_co = st.columns([1, 2, 1])
-    with cent_co:
-        st.image("assets/title.png")
-
-    authenticator = CognitoAuthenticator(
-        pool_id=st.secrets["pool_id"],
-        app_client_id=st.secrets["app_client_id"],
-        app_client_secret=st.secrets["app_client_secret"],
-    )
-
-    st.session_state.login = authenticator.login()
-    components.html(
-        f'<center><a href="{st.secrets["signup_url"]}" target="_blank"> Sign Up </a></center>'
-    )
-
-    if not st.session_state.login:
-        st.stop()
-
-    st.session_state.user_id = authenticator.get_username()
-    st.rerun()
-else:
-    if st.session_state.api_key:
-        openai.api_key = st.session_state.api_key
-    else:
-        openai.api_key = st.secrets["OPEN_AI_KEY"]
-    selected = option_menu(
-        "ふしぎえほん.ai",
-        ["よむ", "つくる"],
-        icons=["bi-play-btn", "bi-brush"],
-        menu_icon=None,
-        default_index=0,
-        orientation="horizontal",
-        styles={
-            "container": {
-                "margin": "0px !important",
-                "background-color": "#fafafa",
-            },
-            "icon": {"color": "fafafa", "font-size": "25px"},
-            "nav-link": {
-                "font-size": "20px",
-                "margin": "0px",
-                "--hover-color": "#eee",
-            },
-            "nav-link-selected": {"background-color": "004a55"},
-        },
-    )
-
-    if selected == "つくる":
-        create()
-    elif selected == "よむ":
-        play()
-    elif selected == "せってい":
-        settings()
+main()
---
+++ modules/streamlit_google_oauth.py
@@ -0,0 +1,113 @@
+import asyncio
+import os
+
+import streamlit as st
+from httpx_oauth.clients.google import GoogleOAuth2
+import streamlit_antd_components as sac
+
+
+async def write_authorization_url(client, redirect_uri):
+    authorization_url = await client.get_authorization_url(
+        redirect_uri,
+        scope=["profile", "email"],
+        extras_params={"access_type": "offline"},
+    )
+    return authorization_url
+
+
+async def write_access_token(client, redirect_uri, code):
+    token = await client.get_access_token(code, redirect_uri)
+    return token
+
+
+async def get_email(client, token):
+    user_id, user_email = await client.get_id_email(token)
+    return user_id, user_email
+
+
+def google_oauth2_required(func):
+    def wrapper(*args, **kwargs):
+        title_cols = st.columns([1, 3, 1])
+        client_id = st.secrets["GOOGLE_CLIENT_ID"]
+        client_secret = st.secrets["GOOGLE_CLIENT_SECRET"]
+        redirect_uri = st.secrets["REDIRECT_URI"]
+
+        client = GoogleOAuth2(client_id, client_secret)
+        authorization_url = asyncio.run(
+            write_authorization_url(client=client, redirect_uri=redirect_uri)
+        )
+
+        if "token" not in st.session_state:
+            st.session_state.token = None
+
+        if st.session_state.token is None:
+            try:
+                code = st.experimental_get_query_params()["code"]
+            except:
+                with title_cols[1]:
+                    st.image("assets/title.png")
+                sac.buttons(
+                    [
+                        sac.ButtonsItem(
+                            label="Googleでログイン",
+                            icon="google",
+                            href=authorization_url,
+                        ),
+                    ],
+                    format_func="title",
+                    align="center",
+                    size="large",
+                )
+            else:
+                # Verify token is correct:
+                try:
+                    token = asyncio.run(
+                        write_access_token(
+                            client=client, redirect_uri=redirect_uri, code=code
+                        )
+                    )
+                except:
+                    with title_cols[1]:
+                        st.image("assets/title.png")
+                        sac.buttons(
+                            [
+                                sac.ButtonsItem(
+                                    label="Googleでログイン",
+                                    icon="google",
+                                    href=authorization_url,
+                                ),
+                            ],
+                            format_func="title",
+                            align="center",
+                            size="large",
+                        )
+                else:
+                    # Check if token has expired:
+                    if token.is_expired():
+                        if token.is_expired():
+                            with title_cols[1]:
+                                st.image("assets/title.png")
+                                sac.buttons(
+                                    [
+                                        sac.ButtonsItem(
+                                            label="Googleでログイン",
+                                            icon="google",
+                                            href=authorization_url,
+                                        ),
+                                    ],
+                                    format_func="title",
+                                    align="center",
+                                    size="large",
+                                )
+                    else:
+                        st.session_state["token"] = token
+                        user_id, user_email = asyncio.run(
+                            get_email(client=client, token=token["access_token"])
+                        )
+                        st.session_state.user_id = user_id
+                        st.session_state.user_email = user_email
+                        func(*args, **kwargs)
+        else:
+            func(*args, **kwargs)
+
+    return wrapper
