8a563cf238427b07f8770bd6e4716abf7055c627
papasim824 <example@example.com>
2023-12-27 18:38:28 +0900

    mod

--- modules/ai.py
+++ modules/ai.py
@@ -52,7 +52,7 @@ def create_tales(
                 break
             except Exception as e:
                 print(e.args)
-                print(f"生成された内容：{content_text}")
+                print(f"生成された内容：{content}")
                 continue
 
     if tales:
