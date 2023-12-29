4dcc61e46452e02e5ca2e0972412d2da913c9bee
ppspps824 <34480286+ppspps824@users.noreply.github.com>
2023-12-29 09:33:56 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -228,8 +228,8 @@ def main():
             st.caption("クレジット購入はこちらから")
             button(username="papasim824C", floating=False,bg_color="#004a55",font_color="#FFFFFF",coffee_color="#FFFFFF")
         ## ライセンス表記
-        st.caption("© 2023 ふしぎえほん.ai All Rights Reserved.")
-        st.caption("Contact papasim824@gmail.com")
+        st.caption("© 2023- ふしぎえほん.ai All Rights Reserved.")
+        st.caption("Contact fushigiehon@gmail.com")
     if selected == "つくる":
         create()
     elif selected == "よむ":
