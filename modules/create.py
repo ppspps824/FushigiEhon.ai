32d0b5d7d14b6d318c716b3018633e88bb46cc85
ppspps824 <34480286+ppspps824@users.noreply.github.com>
2024-01-03 14:25:45 +0900

    create.py を更新

--- modules/create.py
+++ modules/create.py
@@ -398,6 +398,7 @@ def view_edit():
                                 check_credits(st.session_state.user_id, events)
                                 show_overlay()
                                 st.session_state.images["title"] = image_upgrade(
+                                    st.session_state.images["title"],
                                     json.dumps(st.session_state.tales["characters"]),
                                     json.dumps(st.session_state.tales["content"]),
                                     st.session_state.user_id
