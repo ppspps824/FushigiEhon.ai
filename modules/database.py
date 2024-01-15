beab9e155e44e46ae0e049bf636c3628b0e08187
papasim824 <example@example.com>
2024-01-15 20:36:45 +0900

    mod

--- modules/database.py
+++ modules/database.py
@@ -23,7 +23,9 @@ def adding_credits(user_id: str, event: str, value: int = 1):
     """クレジット消費を記録する"""
     if event == "新規登録":
         user_credits_info = read_credits(user_id)
-        if "新規登録" in [info["event"] for info in user_credits_info.data]:
+        events = [info["event"] for info in user_credits_info.data]
+        print(events)
+        if "新規登録" in events:
             return None
     now = datetime.now(jst_tz).isoformat()
     data = {
